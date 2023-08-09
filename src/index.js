6ae30bc8fbc69b6d5d9b6edbd338a3ec72e7f14a
phanhuuviet <86942377+phanhuuviet@users.noreply.github.com>
2023-08-10 01:39:49 +0700

    feat: create model and connect to db

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const { default: mongoose } = require("mongoose");
 
 const app = express();
 const port = process.env.PORT || 3001;
+
+mongoose
+    .connect("mongodb://localhost:27017")
+    .then(() => {
+        console.log("Connected to database");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
 
 app.get("/", (req, res) => {
     res.send("Hello World!");
