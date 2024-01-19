a58d3eea77efd37234ae3d5d4b4e5ead98654f62
phanhuuviet <86942377+phanhuuviet@users.noreply.github.com>
2024-01-19 23:14:24 +0700

    bugfix/API-add-to-cart

--- src/models/CartModel.js
+++ src/models/CartModel.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const cartSchema = new mongoose.Schema(
     {
         userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+        shopId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
         name: { type: String, required: true },
         amount: { type: Number, required: true },
         image: { type: String, required: true },
