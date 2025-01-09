ebcc7c9ebffed6db9e3217cd68b1d96aa4e9ea51
phanhuuviet <86942377+phanhuuviet@users.noreply.github.com>
2025-01-09 23:50:16 +0700

    update/API-login: add middle decoded payload

--- src/routes/loginRouter.js
+++ src/routes/loginRouter.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/authController");
+const { decodePayloadMiddleware } = require("../middlewares/decodePayload");
 
-router.post("/", authController.login);
+router.post("/", decodePayloadMiddleware, authController.login);
 router.post("/login_noSqlInjection", authController.login_noSqlInjection);
 router.post(
     "/login_withPreventNoSqlInjection",
